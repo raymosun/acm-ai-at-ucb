f233cc086840bffe4759803696335fe922e7b2a0
3dcantaloupe <pranavdgs@gmail.com>
2024-06-22 23:37:07 -0700

    added a text box, but idk how to get the text to make the hume config

--- history-chat/src/App.tsx
+++ history-chat/src/App.tsx
@@ -1,12 +1,35 @@
 import './App.css'
 import { useHume } from './lib/useHume';
+import React, { useState } from 'react';
 
 function App() {
-  const messages = useHume();
+  
+  const [text, setText] = useState<string>('');
+  const [displayText, setDisplayText] = useState<string>('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    setDisplayText(text);
+
+  };
+  const messages = useHume(text);
 
   return (
     <>
       <h1>History Chat</h1>
+      <div>
+      <input 
+        type="text" 
+        value={text} 
+        onChange={handleInputChange} 
+        placeholder="Input a historical figure here" 
+      />
+      <button onClick={handleButtonClick}>Submit</button>
+      <p>You have selected: {displayText}</p>
+    </div>
       <div>
         {messages.map((message, index) => (
           <div key={index}>{message}</div>
