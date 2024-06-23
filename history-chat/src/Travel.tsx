0ebaae04dbc582c2f0b4f48e3edfded3728ec493
Sean <ussa.say@gmail.com>
2024-06-22 21:31:39 -0700

    format

--- history-chat/src/Travel.tsx
+++ history-chat/src/Travel.tsx
@@ -7,7 +7,7 @@ interface Person {
   name: string;
   description: string;
   relationship: string;
-  voice: 'masculine' | 'feminine';
+  voice: "masculine" | "feminine";
 }
 
 interface Setting {
@@ -22,47 +22,50 @@ function Travel() {
   const generated = useRef(false);
 
   const [searchParams] = useSearchParams();
-  const prompt = searchParams.get('prompt');
+  const prompt = searchParams.get("prompt");
 
   useEffect(() => {
     // prevent duplicate generation
     if (generated.current) return;
     generated.current = true;
 
-    getSetting(prompt ?? '').then(out => {
-      console.log(out)
+    getSetting(prompt ?? "").then((out) => {
+      console.log(out);
       if (!out) return setSetting(null);
-      setSetting(JSON.parse(out))
+      setSetting(JSON.parse(out));
     });
   }, []);
 
   useEffect(() => {
-    getDialogue(JSON.stringify(setting)).then(out => {
+    getDialogue(JSON.stringify(setting)).then((out) => {
       if (out) {
         setDialogue(JSON.parse(out));
       }
     });
-  }, [setting])
+  }, [setting]);
 
-  return <>
-  <h1>History Chat</h1>
-  <h4>{prompt}</h4>
-  <h4>{setting?.setting}</h4>
-  <div style={{display: 'flex'}}>
-    <div>
-      {JSON.stringify(setting?.personA ?? {})}
-    </div>
-    <div>
-      {JSON.stringify(setting?.personB ?? {})}
-    </div>
-  </div>
-  {dialogue.map((line, index) => (
-    <p key={index}>
-      {index % 2 === 0 ? <b>{setting?.personA.name}</b> : <b>{setting?.personB.name}</b>}:&nbsp;
-      {line}
-    </p>)
-  )}
-  </>
+  return (
+    <>
+      <h1>History Chat</h1>
+      <h4>{prompt}</h4>
+      <h4>{setting?.setting}</h4>
+      <div style={{ display: "flex" }}>
+        <div>{JSON.stringify(setting?.personA ?? {})}</div>
+        <div>{JSON.stringify(setting?.personB ?? {})}</div>
+      </div>
+      {dialogue.map((line, index) => (
+        <p key={index}>
+          {index % 2 === 0 ? (
+            <b>{setting?.personA.name}</b>
+          ) : (
+            <b>{setting?.personB.name}</b>
+          )}
+          :&nbsp;
+          {line}
+        </p>
+      ))}
+    </>
+  );
 }
 
 export default Travel;
