36155d084e0dc4ed4d8302dbf7c357ab0034dbf9
3dcantaloupe <pranavdgs@gmail.com>
2024-06-22 23:40:20 -0700

    Revert "added a text box, but idk how to get the text to make the hume config"
    This reverts commit f233cc086840bffe4759803696335fe922e7b2a0.

--- history-chat/src/App.tsx
+++ history-chat/src/App.tsx
@@ -1,35 +1,12 @@
 import './App.css'
 import { useHume } from './lib/useHume';
-import React, { useState } from 'react';
 
 function App() {
-  
-  const [text, setText] = useState<string>('');
-  const [displayText, setDisplayText] = useState<string>('');
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setText(event.target.value);
-  };
-
-  const handleButtonClick = () => {
-    setDisplayText(text);
-
-  };
-  const messages = useHume(text);
+  const messages = useHume();
 
   return (
     <>
       <h1>History Chat</h1>
-      <div>
-      <input 
-        type="text" 
-        value={text} 
-        onChange={handleInputChange} 
-        placeholder="Input a historical figure here" 
-      />
-      <button onClick={handleButtonClick}>Submit</button>
-      <p>You have selected: {displayText}</p>
-    </div>
       <div>
         {messages.map((message, index) => (
           <div key={index}>{message}</div>
